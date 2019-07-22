ff524a9754f1782c091b8fd81c4a2f9726d41fd9
Michael Wenz <wenzm9@gmail.com>
2019-07-22 21:35:40 +0800

    update

--- if_elif_statements.py
+++ if_elif_statements.py
@@ -1,15 +1,31 @@
 import random
 
 number_list = [random.randint(1, 10) for i in range(10)]
-print(number_list)
 
 def x():
     num = random.randint(1, len(number_list)-1)
-    print(num)
+    #print(num)
     return num
 
-a = number_list[x()]
-b = number_list[x()]
-c = number_list[x()]
+def abc(n):
+    for i in range(n):
+        a = number_list[x()]
+        b = number_list[x()]
+        c = number_list[x()]
 
-print(f'a {a} b {b} c {c}')
+        print(f'loop {n} \t\t a {a} b {b} c {c}')
+
+        if a > b:
+            print('a is greater than b')
+        elif a < b:
+            print('b is greater than a')
+        elif c > b > a:
+            print('c is the greatest ')
+        else:
+            print('this is the last line in the loop')
+            
+abc(4)
+
+    
+
+    
