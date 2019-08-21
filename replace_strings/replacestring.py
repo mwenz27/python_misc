98c161a46feeaf897f56c448dace3d50667eac02
mwenz27 <wenzm9@gmail.com>
2019-08-21 22:06:54 +0800

    functions for conversions

---
+++ replace_strings/replacestring.py
@@ -0,0 +1,11 @@
+string = "geeks for geeks geeks geeks geeks"
+
+string = string.replace("geeks", "fish")
+
+print(string)
+
+new_line = "     1    66588.856   263816.480      23.379     66586.449  263826.195      28.360     0.0000       0    90.2187      0    94.5000      0   105.6562     0.00"
+
+new_line2 = "   12    66588.856   263816.480      23.379      66586.449  263826.195       6.360    11.3750      -9088    11.5625      57550    11.7812     -81830    11.9687     1.00"
+
+print(new_line.replace("66588.856", "INSERT HERE"))
